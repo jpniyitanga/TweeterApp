baa99eb238a718022808266ed3878bc5e3a2b833
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-03-19 23:07:08 -0400

    Add script for a new tweet.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,29 @@
+
+
+$(document).ready(function() {
+  // alert('Welcome');
+});
+
+// $('.textarea').on ('click', function(event) {
+//   // alert("You clicked me");
+//   console.log(this);
+// });
+
+
+$('.textarea').on ('keypress keyup', function(event){
+  $text = $(this).val();
+  $charactersLeft = 140 - $text.length;
+
+  $counter = $(this).closest('form').find('.counter');
+  $counter.text($charactersLeft);
+
+  if ($charactersLeft < 0) {
+    $counter.addClass('redFont');
+  } else {
+    $counter.removeClass('redFont');
+  }
+
+  console.log($text);
+});
+
+
