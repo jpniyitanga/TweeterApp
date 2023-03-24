1689c3a6b6096573055208878569f8dd704c4624
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-03-24 00:07:43 -0400

    Move code inside document.ready function.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,23 @@
 
 
 $(document).ready(function() {
+  $('.textarea').on ('keypress keyup', function(event){
+    $text = $(this).val();
+    $charactersLeft = 140 - $text.length;
+  
+    $counter = $(this).closest('form').find('.counter');
+    $counter.text($charactersLeft);
+  
+    if ($charactersLeft < 0) {
+      $counter.addClass('redFont');
+    } else {
+      $counter.removeClass('redFont');
+    }
+  
+    console.log($text);
+  });
+
+
   // alert('Welcome');
 });
 
@@ -10,20 +27,5 @@ $(document).ready(function() {
 // });
 
 
-$('.textarea').on ('keypress keyup', function(event){
-  $text = $(this).val();
-  $charactersLeft = 140 - $text.length;
-
-  $counter = $(this).closest('form').find('.counter');
-  $counter.text($charactersLeft);
-
-  if ($charactersLeft < 0) {
-    $counter.addClass('redFont');
-  } else {
-    $counter.removeClass('redFont');
-  }
-
-  console.log($text);
-});
 
 
