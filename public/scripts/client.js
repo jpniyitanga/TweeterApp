68b10248983179b5dbc76066a4c28f4b425e0706
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-03-27 01:00:26 -0400

    Implement loadTweets function and move all code inside document.ready function.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,85 +4,65 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-const tweets = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
-// const tweetData123 = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//   "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//   "created_at": 1461116232227
-// }
-
-const createTweetElement = (tweetData) => {
-  const $tweet = $('<article>').addClass('tweet');
-  const html = (`
-    <header>               
-      <div>${tweetData.user.avatars}<span>${tweetData.user.name}</span> </div>              
-      <div class="handle">${tweetData.user.handle}</div>            
-    </header>
-    <p>${tweetData.content.text}</p>
-    <footer>
-      <div>${tweetData.created_at}</div>
-      <div class="icons">
-        <span class="flag"> <a href="#"><i class="fa-solid fa-flag"></i></a></i></span> &nbsp;
-        <span class="retweet"> <a href="#"><i class="fa-solid fa-retweet"></i></a> </span> &nbsp;
-        <span class="like"> <a href="#"><i class="fa-solid fa-heart"></i></a></span>
-      </div>
-    </footer>
-  `);
-  const tweetElement = $tweet.append(html);
-  return tweetElement;
-}
-// console.log(createTweetElement(tweetData123));
-
-const renderTweets = (tweets) => {
-  let tweetContainer = $('.tweets');
- for (const tweet of tweets) {
-   let tweetElement = createTweetElement(tweet);
-  tweetContainer.append(tweetElement);
- }
-}
-
-
 $(document).ready(function () {
-  console.log("ready")
-  const $form = $("form");
-  $form.submit(function(e) {
+
+  const createTweetElement = (tweetData) => {
+    const html = `
+      <article>
+      <div class="tweets">
+        <header>               
+          <div><img src="${tweetData.user.avatars}" alt=""> <span>${tweetData.user.name}</span> </div>              
+          <div class="handle">${tweetData.user.handle}</div>            
+        </header>
+        <p class="text">${tweetData.content.text}</p>
+        <footer>
+        <div>${timeago.format(tweetData.created_at)}</div>
+        <div class="icons">
+          <span class="flag"> <a href="#"><i class="fa-solid fa-flag"></i></a></i></span> &nbsp;
+          <span class="retweet"> <a href="#"><i class="fa-solid fa-retweet"></i></a> </span> &nbsp;
+          <span class="like"> <a href="#"><i class="fa-solid fa-heart"></i></a></span>
+        </div>
+        </footer>
+      </div>    
+    </article>
+    `;
+
+    return html;
+  };
+
+  const renderTweets = (tweets) => {
+    const tweetContainer = $(".tweets-container");
+    for (const tweet of tweets) {
+      const tweetElement = createTweetElement(tweet);
+      tweetContainer.prepend(tweetElement);
+    }
+  };
+
+  const loadTweets = function () {
+    $.ajax({
+      url: "http://localhost:8080/tweets",
+      method: "GET",
+      dataType: "json",
+      success: function (data) {
+        renderTweets(data);
+      },
+    });
+  };
+
+  $("form").submit(function (e) {
     e.preventDefault();
-  
+
     const $formData = $("textarea").serialize();
-    $.post('/tweets', $formData, function(response) {
-      console.log("response", response);
-    })
-  
-  })
+    $.ajax({
+      url: "http://localhost:8080/tweets",
+      method: "POST",
+      data: $formData,
+      success: function () {
+        console.log("Success");
+        loadTweets();
+      },
+    });
+  });
+
+  loadTweets()
 });
